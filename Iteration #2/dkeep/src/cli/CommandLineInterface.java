05327eafffb9e78550a270625170b95497f67259
Kyahra <sofiacbahamonde@gmail.com>
2017-03-04 15:15:48 +0000

    Guard captures hero

--- Iteration #2/dkeep/src/cli/CommandLineInterface.java
+++ Iteration #2/dkeep/src/cli/CommandLineInterface.java
@@ -1,6 +1,7 @@
 package cli;
 
 import logic.Game;
+import logic.Game.GameState;
 
 
 public class CommandLineInterface {
@@ -10,14 +11,39 @@ public class CommandLineInterface {
 	
 		while(true){
 
-
 		g.updateMap();
 		g.printMap();
 		g.cleanMap();
+		
+		if(g.getState() == GameState.LOST || g.getState() == GameState.WON)
+			break;
+		
 		g.readMove();
 		g.updateGame();
 		
 		
+		}
+		
+		if(g.getState() == GameState.LOST){
+			System.out.println();
+			System.out.println();
+			
+			System.out.print("--------------");
+			System.out.print(" GAME OVER ");
+			System.out.print("--------------");
+			
+		}
+		
+		if(g.getState() == GameState.WON){
+			System.out.println();
+			System.out.println();
+			System.out.println();
+			
+			System.out.print("---------------");
+			System.out.print(" VICTORY ");
+			System.out.print("---------------");
+			
+			
 		}
 	}
 	
--- Iteration #2/dkeep/src/logic/Game.java
+++ Iteration #2/dkeep/src/logic/Game.java
@@ -7,24 +7,25 @@ import java.awt.Point;
 import java.util.Scanner;
 
 public class Game {
-	
-	public enum GameState { WON, LEVEL_1, LEVEL_2, LOST };
 
+	public enum GameState {
+		WON, LEVEL_1, LEVEL_2, LOST
+	};
 
 	private char[][] map_1 = { 
 			{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
 			{ 'X', ' ', ' ', ' ', 'I', ' ', 'X', ' ', ' ', 'X' }, 
 			{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
-			{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X' },
+			{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', ' ', ' ', 'X' }, 
 			{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', ' ', ' ', 'X' },
 			{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
 			{ 'I', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-			{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X' },
+			{ 'X', 'X', 'X', ' ', 'X', 'X', 'X', 'X', ' ', 'X' }, 
 			{ 'X', ' ', 'I', ' ', 'I', ' ', 'X', 'k', ' ', 'X' },
 			{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
 			};
 
-	private char[][] map_2 = { 
+	private char[][] map_2 = {
 			{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' },
 			{ 'I', ' ', ' ', ' ', 'O', ' ', ' ', ' ', 'k', 'X' }, 
 			{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
@@ -32,7 +33,7 @@ public class Game {
 			{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
 			{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
 			{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
-			{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
+			{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' }, 
 			{ 'X', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', 'X' },
 			{ 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X' }, 
 			};
@@ -47,19 +48,17 @@ public class Game {
 	private Guard guard;
 	private Character lever;
 
-
 	public Game() {
 		state = GameState.LEVEL_1;
-
 
 		Point hero_init_pos = new Point(1, 1);
 		hero = new Hero(hero_init_pos, 'H');
 
 		Point guard_init_pos = new Point(8, 1);
 		guard = new Guard(guard_init_pos, 'G');
-		
-		Point lever_init_pos = new Point(7,8);
-		lever = new Character(lever_init_pos,'k');
+
+		Point lever_init_pos = new Point(7, 8);
+		lever = new Character(lever_init_pos, 'k');
 
 	}
 
@@ -109,7 +108,6 @@ public class Game {
 
 	}
 
-
 	public void drawCharacter(Character c) {
 
 		switch (state) {
@@ -135,26 +133,25 @@ public class Game {
 		Scanner keyboard = new Scanner(System.in);
 
 		String input = keyboard.nextLine().toUpperCase();
-		
+
 		direction = input.charAt(0);
 
-		
 	}
-	
-	public void updateGame(){
+
+	public void updateGame() {
 		Point new_hero_pos;
-		
+
 		new_hero_pos = hero.getNewPosition(direction);
 		hero.updateHero(getChar(new_hero_pos));
-	
-		if(hero.getState() == HeroState.LEVER)openDoors();
-				
 
-		guard.updateGuard();		
+		if (hero.getState() == HeroState.LEVER)
+			openDoors();
+
+		guard.updateGuard();
 
 	}
-	
-	public void cleanCharacter(Character c){
+
+	public void cleanCharacter(Character c) {
 		switch (state) {
 		case LEVEL_1:
 			map_1[c.getY()][c.getX()] = ' ';
@@ -164,28 +161,32 @@ public class Game {
 			break;
 		}
 	}
-	
-	public void cleanMap(){
+
+	public void cleanMap() {
 		cleanCharacter(hero);
-		if(hero.getState() == HeroState.LEVER)drawCharacter(lever);
-		if(hero.getState() == HeroState.STAIR)state = GameState.LEVEL_2;
+		if (hero.getState() == HeroState.LEVER)
+			drawCharacter(lever);
+		if (hero.getState() == HeroState.STAIR)
+			state = GameState.LEVEL_2;
 		
-			
+		if(isHeroCaptured(guard))
+			state = GameState.LOST;
+
 		cleanCharacter(guard);
 	}
-	
-	public void openDoors(){
-		
-		switch(state){
+
+	public void openDoors() {
+
+		switch (state) {
 		case LEVEL_1:
-		for (int i = 0; i < rows; i++) {
-			for (int j = 0; j < cols; j++) {
-				if (map_1[i][j] == 'I')
-					map_1[i][j] = 'S';
+			for (int i = 0; i < rows; i++) {
+				for (int j = 0; j < cols; j++) {
+					if (map_1[i][j] == 'I')
+						map_1[i][j] = 'S';
+				}
 			}
-		}
-		break;
-		
+			break;
+
 		case LEVEL_2:
 			for (int i = 0; i < rows; i++) {
 				for (int j = 0; j < cols; j++) {
@@ -194,9 +195,31 @@ public class Game {
 				}
 			}
 			break;
-			
+
 		default:
 			break;
 		}
+	}
+
+	public boolean isHeroCaptured(Character enemy) {
+
+		if (hero.getX() == enemy.getX() + 1 && hero.getY() == enemy.getY())
+			return true;
+
+		if (hero.getX() == enemy.getX() - 1 && hero.getY() == enemy.getY())
+			return true;
+
+		if (hero.getX() == enemy.getX() && hero.getY() == enemy.getY() + 1)
+			return true;
+
+		if (hero.getX() == enemy.getX() && hero.getY() == enemy.getY() - 1)
+			return true;
+
+		return false;
+	}
+	
+	
+	public GameState getState(){
+		return state;
 	}
 }
