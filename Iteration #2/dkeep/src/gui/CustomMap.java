df908925e54e3b0b3449d363b874f58e2d24d88e
dolfander <daniel.pmachado@hotmail.com>
2017-03-24 18:18:28 +0000

    Create Map Buttons

--- Iteration #2/dkeep/src/gui/CustomMap.java
+++ Iteration #2/dkeep/src/gui/CustomMap.java
@@ -4,6 +4,8 @@ import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.SwingConstants;
 
 public class CustomMap extends JPanel{
 	
@@ -14,24 +16,31 @@ public class CustomMap extends JPanel{
 		setBackground(java.awt.Color.BLACK);
 		setLayout(null);
 		
-		JButton btnWall = new JButton("Wall");
-		btnWall.setBounds(24, 39, 63, 43);
+		JButton btnWall = new JButton("");
+		btnWall.setHorizontalTextPosition(SwingConstants.CENTER);
+		btnWall.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconWall.png")));
+		btnWall.setBackground(Color.BLACK);
+		btnWall.setBounds(35, 40, 49, 49);
 		add(btnWall);
 		
-		JButton btnExit = new JButton("Exit");
-		btnExit.setBounds(104, 40, 63, 40);
+		JButton btnExit = new JButton("");
+		btnExit.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconDoor.png")));
+		btnExit.setBounds(111, 40, 49, 49);
 		add(btnExit);
 		
-		JButton btnKey = new JButton("key");
-		btnKey.setBounds(198, 40, 76, 40);
+		JButton btnKey = new JButton("");
+		btnKey.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconKey.png")));
+		btnKey.setBounds(198, 40, 49, 49);
 		add(btnKey);
 		
-		JButton btnHero = new JButton("hero");
-		btnHero.setBounds(302, 40, 63, 40);
+		JButton btnHero = new JButton("");
+		btnHero.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconHero.png")));
+		btnHero.setBounds(275, 40, 49, 49);
 		add(btnHero);
 		
-		JButton btnOgre = new JButton("Ogre\r\n");
-		btnOgre.setBounds(401, 40, 63, 40);
+		JButton btnOgre = new JButton("");
+		btnOgre.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconOgre.png")));
+		btnOgre.setBounds(361, 40, 49, 49);
 		add(btnOgre);
 
 		
