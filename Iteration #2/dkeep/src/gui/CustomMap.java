a67a0c012aa12de6e081cf04005e6fd0c41e2852
Kyahra <sofiacbahamonde@gmail.com>
2017-03-24 23:06:47 +0000

    Custom Map updated

--- Iteration #2/dkeep/src/gui/CustomMap.java
+++ Iteration #2/dkeep/src/gui/CustomMap.java
@@ -7,18 +7,18 @@ import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.ImageIcon;
 import java.awt.Color;
+import java.awt.Image;
+
 import javax.swing.SwingConstants;
 
 
 
 import java.awt.event.MouseListener;
 
-import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
-import javax.swing.JPanel;
-import java.awt.Color;
+
 
 public class CustomMap  extends JPanel implements MouseListener  {
 
@@ -35,8 +35,10 @@ public class CustomMap  extends JPanel implements MouseListener  {
 		setHeroButton();
 		setPlayButton();
 		setBackButton();
+		setBackground(java.awt.Color.BLACK);
 		
 		GamePanel panel = new GamePanel(350,350,rows,cols);
+		panel.setBackground(Color.GREEN);
 		panel.setBounds(20, 72, 317, 327);
 		add(panel);
 		
@@ -60,95 +62,71 @@ public class CustomMap  extends JPanel implements MouseListener  {
 
 	private void setHeroButton() {
 		JButton button_3 = new JButton("Hero");
+		button_3.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconHero.png")));
 		button_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				c='H';
 			}
 		});
-		button_3.setBounds(367, 343, 62, 56);
+		button_3.setBounds(367, 340, 49, 59);
 		add(button_3);
 		
-
-
-		setBackground(java.awt.Color.BLACK);
-		setLayout(null);
-		
-		JButton btnWall = new JButton("");
-		btnWall.setHorizontalTextPosition(SwingConstants.CENTER);
-		btnWall.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconWall.png")));
-		btnWall.setBackground(Color.BLACK);
-		btnWall.setBounds(38, 40, 49, 49);
-		add(btnWall);
-		
-		JButton btnExit = new JButton("");
-		btnExit.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconDoor.png")));
-		btnExit.setBounds(120, 40, 49, 49);
-		add(btnExit);
-		
-
+	
 	}
 
 	private void setOgreButton() {
 		JButton button_2 = new JButton("Ogre");
+		button_2.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconOgre.png")));
 		button_2.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				c='O';
 			}
 		});
-		button_2.setBounds(367, 261, 62, 56);
+		button_2.setBounds(367, 265, 49, 52);
 		add(button_2);
 		
 	}
 
 	private void setKeyButton() {
 		JButton button_1 = new JButton("Key");
+		button_1.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconKey.png")));
 		button_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				c= 'k';
 			}
 		});
-		button_1.setBounds(367, 187, 62, 52);
+		button_1.setBounds(367, 187, 49, 52);
 		add(button_1);
 		
 		
 	}
 
-		JButton btnKey = new JButton("");
-		btnKey.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconKey.png")));
-		btnKey.setBounds(197, 40, 49, 49);
-		add(btnKey);
-		
-		JButton btnHero = new JButton("");
-		btnHero.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconHero.png")));
-		btnHero.setBounds(279, 40, 49, 49);
-		add(btnHero);
-		
-		JButton btnOgre = new JButton("");
-		btnOgre.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconOgre.png")));
-		btnOgre.setBounds(356, 40, 49, 49);
-		add(btnOgre);
 
 
 	private void setExitButton() {
 		JButton button = new JButton("Exit");
+		button.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconDoor.png")));
 		button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				c='I';
 			}
 		});
-		button.setBounds(367, 124, 62, 52);
+		button.setBounds(367, 122, 49, 54);
 		add(button);
 		
 	}
 
 	private void setWallButton() {
-		JButton btnNewButton = new JButton("Wall");
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setHorizontalAlignment(SwingConstants.RIGHT);
+		btnNewButton.setVerticalAlignment(SwingConstants.TOP);
+		btnNewButton.setIcon(new ImageIcon(CustomMap.class.getResource("/gui/res/iconWall.png")));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				c='X';
 			}
 		});
-		btnNewButton.setBounds(367, 61, 62, 52);
+		btnNewButton.setBounds(357, 74, 59, 52);
 		add(btnNewButton);
 		
 	}
@@ -180,4 +158,6 @@ public class CustomMap  extends JPanel implements MouseListener  {
 		// TODO Auto-generated method stub
 		
 	}
+	
+
 }
