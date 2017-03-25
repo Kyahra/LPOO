4dac4fef6e128333e60b6deecf1f4a790f294b10
dolfander <daniel.pmachado@hotmail.com>
2017-03-25 23:51:53 +0000

    GameBar working

--- Iteration #2/dkeep/src/gui/GameBar.java
+++ Iteration #2/dkeep/src/gui/GameBar.java
@@ -9,70 +9,94 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.ImageIcon;
 import javax.swing.border.MatteBorder;
 import java.awt.Component;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class GameBar extends JPanel {
 
+	static GamePanel pnlGame;
+	
 	/**
 	 * Create the panel.
 	 */
 	public GameBar() {
 		setBackground(Color.BLACK);
 		
-		JButton btnNewButton = new JButton("");
-		btnNewButton.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
-		btnNewButton.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/arrowUp.png")));
+		JButton up = new JButton("");
+		up.setBounds(437, 507, 33, 33);
+		up.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		up.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/arrowUp.png")));
+		up.setBackground(Color.BLACK);
+		
+		JButton left = new JButton("");
+		left.setBounds(410, 533, 33, 33);
+		left.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		left.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/arrowLeft.png")));
+		left.setBackground(Color.BLACK);
+		left.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		
+		JButton down = new JButton("");
+		down.setBounds(440, 559, 29, 25);
+		down.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/arrowDown.png")));
+		down.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		down.setBackground(Color.BLACK);
+		
+		JButton right = new JButton("");
+		right.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		right.setBounds(465, 533, 33, 33);
+		right.setAlignmentX(Component.CENTER_ALIGNMENT);
+		right.setBackground(Color.BLACK);
+		right.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/arrowRight.png")));
+		right.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
+		
+		
+		setLayout(null);
+		
+		pnlGame = new GamePanel(400, 400, 10, 10);
+		pnlGame.setBounds(0, 0, 550, 495);
+		this.add(pnlGame);
+		add(down);
+		add(left);
+		add(up);
+		add(right);
+		
+		JButton howToPlay = new JButton("");
+		howToPlay.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				JOptionPane.showMessageDialog(GameWindow.frmMazeGame, " Press keyboard arrow keys to move your hero\n "
+																	+ " around the maze. You can also click on the buttons.\n"
+																	+ " Your goal is to catch the key and then reach the doors.\n"
+																	+ "                                                        \n"
+																	+ "                  Good luck!!!              ");
+			}
+		});
+		howToPlay.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
+		howToPlay.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/rsz_2howtoplay.png")));
+		howToPlay.setBackground(Color.BLACK);
+		howToPlay.setBounds(170, 522, 188, 44);
+		add(howToPlay);
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/rsz_menu.png")));
 		btnNewButton.setBackground(Color.BLACK);
+		btnNewButton.setBounds(30, 517, 111, 49);
+		add(btnNewButton);
 		
-		JButton btnNewButton_1 = new JButton("");
-		btnNewButton_1.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/arrowLeft.png")));
-		btnNewButton_1.setBackground(Color.BLACK);
-		btnNewButton_1.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
-		
-		JButton btnNewButton_2 = new JButton("");
-		btnNewButton_2.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/arrowDown.png")));
-		btnNewButton_2.setBorder(new MatteBorder(1, 1, 1, 1, (Color) new Color(0, 0, 0)));
-		btnNewButton_2.setBackground(Color.BLACK);
-		
-		JButton btnNewButton_3 = new JButton("");
-		btnNewButton_3.setAlignmentX(Component.CENTER_ALIGNMENT);
-		btnNewButton_3.setBackground(Color.BLACK);
-		btnNewButton_3.setIcon(new ImageIcon(GameBar.class.getResource("/gui/res/arrowRight.png")));
-		btnNewButton_3.setBorder(new MatteBorder(2, 2, 2, 2, (Color) new Color(0, 0, 0)));
-		GroupLayout groupLayout = new GroupLayout(this);
-		groupLayout.setHorizontalGroup(
-			groupLayout.createParallelGroup(Alignment.TRAILING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(330)
-					.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-					.addGap(41)
-					.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)
-					.addGap(27))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addContainerGap()
-							.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 29, GroupLayout.PREFERRED_SIZE))
-						.addGroup(groupLayout.createSequentialGroup()
-							.addGap(361)
-							.addComponent(btnNewButton, 0, 0, Short.MAX_VALUE)))
-					.addGap(60))
-		);
-		groupLayout.setVerticalGroup(
-			groupLayout.createParallelGroup(Alignment.LEADING)
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(14)
-					.addComponent(btnNewButton)
-					.addPreferredGap(ComponentPlacement.RELATED)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(btnNewButton_1, 0, 0, Short.MAX_VALUE)
-							.addGap(8)
-							.addComponent(btnNewButton_2, GroupLayout.PREFERRED_SIZE, 25, GroupLayout.PREFERRED_SIZE)
-							.addGap(187))
-						.addComponent(btnNewButton_3, GroupLayout.PREFERRED_SIZE, 27, GroupLayout.PREFERRED_SIZE)))
-		);
-		setLayout(groupLayout);
 
 	}
 
+	public void update() {
+		pnlGame.update();
+		
+	}
 }
