144f198d08513c186535ff305684a638b81b89d6
dolfander <daniel.pmachado@hotmail.com>
2017-03-26 19:25:23 +0100

    Tests 66%

--- Iteration #2/dkeep/src/logic/Guard.java
+++ Iteration #2/dkeep/src/logic/Guard.java
@@ -2,12 +2,16 @@ package logic;
 
 import logic.Character;
 import java.awt.Point;
+import java.util.Random;
 
 public class Guard extends Character{
 
 	protected int[] guard_y= {1,1,2,3,4,5,5,5,5,5,5,5,6,6,6,6,6,6,6,6,5,4,3,2};
 	protected int[] guard_x= {8,7,7,7,7,7,6,5,4,3,2,1,1,2,3,4,5,6,7,8,8,8,8,8};
 	protected int guard_idx;
+	protected boolean isSleeping = false;
+	protected int moveNumber = 4;
+	protected boolean front = true;
 	
 	
 	public Guard(Point position){
@@ -20,5 +24,33 @@ public class Guard extends Character{
 	
 	public void updateGuard(){};
 	
+	public int getIdxDirection(){
+		
+		Random rand = new Random();		
+		int direction = rand.nextInt(4);
+		return direction;
+		
+	}
+	
+	public boolean isSleeping() {
+		return isSleeping;
+	}
+
+	public void setSleeping(boolean isSleeping) {
+		this.isSleeping = isSleeping;
+	}
+
+	public int getMoveNumber() {
+		return moveNumber;
+	}
+
+
+	public boolean isFront() {
+		return front;
+	}
+
+	public void setFront(boolean front) {
+		this.front = front;
+	}
 	
 }
